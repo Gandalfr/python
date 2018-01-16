718d80dffd8c27455f89131ff54cf868df412a56
Gandalfr <1505838700@qq.com>
2018-01-16 13:00:47 +0800

    Vector class

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print ('Hello world!')
+import vector
+
+my_vector = vector.Vector([1,2,3])
+my_vector2 = vector.Vector([1,2,3])
+my_vector3 = vector.Vector([-1,2,3])
+
+print my_vector
+print my_vector2
+print my_vector3
+
+print my_vector == my_vector2
+print my_vector == my_vector3
+
+print my_vector.plus(my_vector2)
+#print my_vector.minus(my_vector2)
+#print my_vector.times_scalar(3)
